00c9c40cbc5ffccce347a78017d78bcb2106c934
Mikhail-Ropii <mr.ropiy@gmail.com>
2022-05-22 14:34:37 +0300

    Update ContactList.jsx

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -5,9 +5,9 @@ import propTypes from 'prop-types';
 export const ContactList = ({ contacts, handleDeleteContact }) => {
   return (
     <ul>
-      {contacts.map(({ id, name, number }) => (
+      {contacts.map(({ id, name, phone }) => (
         <BookItem key={id}>
-          <ContactItem name={name} number={number} />
+          <ContactItem name={name} number={phone} />
           <Button type="button" onClick={() => handleDeleteContact(id)}>
             Delete
           </Button>
